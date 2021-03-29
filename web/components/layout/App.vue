9add546d9ce4f5eaf7fe7d170d20dbc8263ec0b8
yuuangzhang <yuuangzhang@tencent.com>
2021-03-29 19:49:31 +0800

    feat: init

---
+++ web/components/layout/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Button type="primary">
+    主要按钮
+  </Button>
+
+  <router-view />
+</template>
+
+<script>
+import { Button } from 'vant'
+
+export default {
+  components: {
+    Button
+  }
+}
+</script>
